ad52eff55f01fe915ab75d51eb6d5598230968ae
hao <ahmed.ait.ouazzou@protonmail.com>
2024-05-18 00:18:22 +0200

    Added Exploratory Data Analysis step

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,9 +1,10 @@
 from typing import List
 import os
+import io
 import pandas as pd
 
 
-def merge_csv_files(directory: str, output_file: str) -> None:
+def merge_csv_files_by_rows(directory: str, output_file: str) -> pd.DataFrame:
     """
     Merge multiple CSV files from a directory into a single CSV file.
 
@@ -12,7 +13,7 @@ def merge_csv_files(directory: str, output_file: str) -> None:
     output_file (str): The name of the output CSV file.
 
     Returns:
-    None
+    merged_df (pd.DataFrame): The merged pandas dataframe by rows
     """
     # List to store individual DataFrames
     all_dataframes: List[pd.DataFrame] = []
@@ -32,3 +33,39 @@ def merge_csv_files(directory: str, output_file: str) -> None:
 
     # Save the merged DataFrame to a CSV file
     merged_df.to_csv(output_file, index=False)
+
+    return merged_df
+
+
+def merge_csv_files_by_columns(file1_path: str, file2_path: str) -> pd.DataFrame:
+    """
+    Merge two CSV files based on the 'article_id' column from the first file
+    and the 'click_article_id' column from the second file.
+
+    Args:
+        file1_path (str): The path to the first CSV file.
+        file2_path (str): The path to the second CSV file.
+
+    Returns:
+        merged_df (pd.DataFrame): The merged pandas Dataframe by columns
+    """
+    # Read the CSV files into pandas DataFrames
+    df1 = pd.read_csv(file1_path)
+    df2 = pd.read_csv(file2_path)
+
+    # Rename the column in df2 to match the column name in df1
+    df2 = df2.rename(columns={"click_article_id": "article_id"})
+
+    # Merge the two DataFrames based on the 'article_id' column
+    merged_df = pd.merge(df1, df2, how="left", on="article_id")
+
+    # Convert the merged DataFrame to a CSV string
+    csv_buffer = io.StringIO()
+    merged_df.to_csv(csv_buffer, index=False)
+    merged_csv = csv_buffer.getvalue()
+
+    return merged_df
+
+
+def test_function(a, b):
+    return a + b
