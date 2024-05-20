6c174dc5c887974aa22d587aa0b8f8250135f1db
hao <ahmed.ait.ouazzou@protonmail.com>
2024-05-20 21:20:35 +0200

    Added function merge_csv_files and relocated csv files created

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,16 +4,16 @@ import io
 import pandas as pd
 
 
-def merge_csv_files_by_rows(directory: str, output_file: str) -> pd.DataFrame:
+def concat_csv_files(directory: str, output_file: str) -> pd.DataFrame:
     """
-    Merge multiple CSV files from a directory into a single CSV file.
+    Concat multiple CSV files from a directory into a single CSV file.
 
     Args:
-    directory (str): The directory containing CSV files to merge.
+    directory (str): The directory containing CSV files to concat.
     output_file (str): The name of the output CSV file.
 
     Returns:
-    merged_df (pd.DataFrame): The merged pandas dataframe by rows
+    concated_df (pd.DataFrame): The concated pandas dataframe
     """
     # List to store individual DataFrames
     all_dataframes: List[pd.DataFrame] = []
@@ -29,15 +29,15 @@ def merge_csv_files_by_rows(directory: str, output_file: str) -> pd.DataFrame:
             all_dataframes.append(df)
 
     # Merge all DataFrames into one
-    merged_df = pd.concat(all_dataframes, ignore_index=True)
+    concated_df = pd.concat(all_dataframes, ignore_index=True)
 
     # Save the merged DataFrame to a CSV file
-    merged_df.to_csv(output_file, index=False)
+    concated_df.to_csv(output_file, index=False)
 
-    return merged_df
+    return concated_df
 
 
-def merge_csv_files_by_columns(file1_path: str, file2_path: str) -> pd.DataFrame:
+def merge_csv_files(file1_path: str, file2_path: str, output_file: str) -> pd.DataFrame:
     """
     Merge two CSV files based on the 'article_id' column from the first file
     and the 'click_article_id' column from the second file.
@@ -45,6 +45,7 @@ def merge_csv_files_by_columns(file1_path: str, file2_path: str) -> pd.DataFrame
     Args:
         file1_path (str): The path to the first CSV file.
         file2_path (str): The path to the second CSV file.
+        output_file (str): The name of the output CSV file.
 
     Returns:
         merged_df (pd.DataFrame): The merged pandas Dataframe by columns
@@ -53,19 +54,13 @@ def merge_csv_files_by_columns(file1_path: str, file2_path: str) -> pd.DataFrame
     df1 = pd.read_csv(file1_path)
     df2 = pd.read_csv(file2_path)
 
-    # Rename the column in df2 to match the column name in df1
-    df2 = df2.rename(columns={"click_article_id": "article_id"})
-
     # Merge the two DataFrames based on the 'article_id' column
-    merged_df = pd.merge(df1, df2, how="left", on="article_id")
+    merged_df = pd.merge(df1, df2, how="left", left_on="click_article_id",
+                         right_on="article_id")
 
-    # Convert the merged DataFrame to a CSV string
-    csv_buffer = io.StringIO()
-    merged_df.to_csv(csv_buffer, index=False)
-    merged_csv = csv_buffer.getvalue()
+    merged_df = merged_df.drop(columns=['click_article_id'])
+
+    # Save the merged DataFrame to a CSV file
+    merged_df.to_csv(output_file, index=False)
 
     return merged_df
-
-
-def test_function(a, b):
-    return a + b
