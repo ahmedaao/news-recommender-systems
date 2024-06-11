530756f994e365cf7bbea44fb3ae5790d2bcd2ef
hao <ahmed.ait.ouazzou@protonmail.com>
2024-06-11 11:21:07 +0200

    Added function cf_baseline_only to realize predictions

--- src/modeling/predict.py
+++ src/modeling/predict.py
@@ -6,6 +6,48 @@ import pandas as pd
 from src import dataset
 
 
+def cf_baseline_only(
+    ROOT_DIR: str, user_id: int, df: pd.DataFrame, nb_articles_to_print: int
+):
+    # Deserialization pickle object
+    with open(
+        os.path.join(ROOT_DIR, "models/model_baseline_only.pickle"), "rb"
+    ) as file:
+        best_model_baseline_only = pickle.load(file)
+
+    # List all articles not click by a specific user
+    articles_not_clicked_by_user = dataset.articles_not_clicked_by_user(df, user_id)
+
+    # Make predictions
+    predictions = []
+    for article_id in articles_not_clicked_by_user["article_id"]:
+        prediction = best_model_baseline_only.predict(
+            uid=articles_not_clicked_by_user["user_id"], iid=article_id
+        )
+        predictions.append(
+            {
+                "user_id": prediction.uid,
+                "article_id": prediction.iid,
+                "predicted_rating": prediction.est,
+                "details": prediction.details,
+            }
+        )
+
+    # Sort the predictions in descending order by 'predicted_rating'
+    predictions.sort(key=lambda x: x["predicted_rating"], reverse=True)
+    predictions = predictions[:nb_articles_to_print]
+
+    article_ids = [entry["article_id"] for entry in predictions]
+    predicted_ratings = [entry["predicted_rating"] for entry in predictions]
+
+    result = {
+        "article_ids": article_ids,
+        "predicted_ratings": predicted_ratings,
+    }
+
+    return result
+
+
 def cf_knn(ROOT_DIR: str, user_id: int, df: pd.DataFrame, nb_articles_to_print: int):
     # Deserialization pickle object
     with open(os.path.join(ROOT_DIR, "models/model_based_knn.pickle"), "rb") as file:
@@ -35,12 +77,6 @@ def cf_knn(ROOT_DIR: str, user_id: int, df: pd.DataFrame, nb_articles_to_print: 
 
     article_ids = [entry["article_id"] for entry in predictions]
     predicted_ratings = [entry["predicted_rating"] for entry in predictions]
-
-    # Display the predictions
-    # for pred in predictions:
-    #    print(
-    #        f"User ID: {pred['user_id']}, Article ID: {pred['article_id']}, Predicted Rating: {pred['predicted_rating']}, Details: {pred['details']}"
-    #    )
 
     result = {
         "article_ids": article_ids,
--- src/modeling/train.py
+++ src/modeling/train.py
@@ -3,9 +3,64 @@ import pickle
 import pandas as pd
 from surprise import Dataset
 from surprise import Reader
-from surprise import KNNWithMeans
+from surprise import BaselineOnly, KNNWithMeans
 from surprise.model_selection import GridSearchCV, train_test_split
 from surprise import accuracy
+
+
+def model_baseline_only(df: pd.DataFrame, model_filename: str):
+    """
+    Train a collaborative filtering model using BaselineOnly
+    and save the best model.
+
+    Parameters:
+    df (pd.DataFrame): DataFrame containing the user-item ratings.
+    It should have columns corresponding to user IDs, item IDs, and ratings.
+    model_filename (str): The filename to save the trained model as a pickle.
+
+    Returns:
+    best_model: The best trained baseline model based on RMSE.
+    """
+    # Define the rating scale
+    reader = Reader(rating_scale=(0, 10))
+
+    # Loads Pandas Dataframe
+    data = Dataset.load_from_df(df, reader)
+
+    # Split the data into training and testing sets
+    trainset, testset = train_test_split(data, test_size=0.2)
+
+    # Set baseline options
+    bsl_options = {
+        "method": ["als", "sgd"],
+        "n_epochs": [5, 10, 20],
+        "reg_u": [12, 15, 20],
+        "reg_i": [5, 10, 15],
+    }
+
+    param_grid = {"bsl_options": bsl_options}
+
+    # Perform grid search with cross-validation
+    gs = GridSearchCV(BaselineOnly, param_grid, measures=["rmse", "mae"], cv=3)
+    gs.fit(data)
+
+    # Get the best model from the grid search
+    best_model = gs.best_estimator["rmse"]
+
+    # Train the best model on the full training set
+    best_model.fit(trainset)
+
+    # Save the best model to a file using pickle
+    with open(model_filename, "wb") as model_file:
+        pickle.dump(best_model, model_file)
+
+    # Make predictions on the test set
+    predictions = best_model.test(testset)
+
+    # Calculate the RMSE
+    rmse = accuracy.rmse(predictions, verbose=True)
+
+    return best_model, rmse
 
 
 def model_based_knn(df: pd.DataFrame, model_filename: str):
@@ -33,7 +88,7 @@ def model_based_knn(df: pd.DataFrame, model_filename: str):
     # Set similarity options
     sim_options = {
         "name": ["msd"],
-        "min_support": [3],
+        "min_support": [4, 5],
         "user_based": [False],
     }
 
